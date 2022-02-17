f180fcf7ee044d4a03cdbece3ed426b857238e76
Alex Antonyuk <alex@antonyuk.me>
2022-02-17 10:38:26 +0200

    naming

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "NetworkingAddons",
+    name: "NetSecAddons",
     platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
